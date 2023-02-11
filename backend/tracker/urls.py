e9df5e1fad869052e2af7d3815356a584e224954
biohazard2117 <uttu.college.id@gmail.com>
2023-02-11 19:17:25 +0530

    created all apis

--- backend/tracker/urls.py
+++ backend/tracker/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('lights/', LightsAPI.as_view())
+    path('lights/', LightsAPI.as_view()),
+    path('poles/', PolePowerAPI.as_view())
 ]
--- backend/tracker/views.py
+++ backend/tracker/views.py
@@ -9,34 +9,54 @@ from rest_framework import status
 
 class LightsAPI(APIView):
     
+    #get data of specific street light
     def get(self,request):
-        # lights
-        print(request.GET.get('id'))
-        lights_data = StreetLight.objects.filter(id=request.GET.get('id'))
-        # print(lights_data.id)
-        serializer = StreetLightSerializer(data=lights_data, many=True)
-        # print(serializer.data)
-        if not serializer.is_valid():
-            print(serializer.errors)
-            return Response({'message': "No street light found"}, status=status.HTTP_404_NOT_FOUND)
-        # return Response({'message': 'Got Data'})
-        return Response({'message': 'Data retrieved successfully', 'data': serializer.data}, status=status.HTTP_200_OK)
-        # pass 
+        id = request.GET.get('id')
+        if id!=None:
+            streetlight = StreetLight.objects.get(id=id)
+            serializer = StreetLightSerializer(streetlight)
+        return Response({'data': serializer.data}, status=status.HTTP_200_OK)
+
     
     # to register poles 
     def post(self, request):
+        # check if data already exists
+        # id=request.data['id']
+        # streetlight = StreetLight.objects.get(id=id)
+
+        # if streetlight != None:
+        #     return Response({'message':'Data already exists'}, status=status.HTTP_400_BAD_REQUEST)
+        
         serializer = StreetLightSerializer(data = request.data)
-        # print(serializer.data)
         if not serializer.is_valid():
             print ("Data Not valid")
-            return Response({'message': "Data send Not valid"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'message': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
         
         serializer.save()
         # return Response({'message': 'Posted Data'})
         return Response({'data': serializer.data, 'message': 'Data Saved Successfully'}, status=status.HTTP_201_CREATED)
     
+    # update uptime and status 
+    def patch(self, request):
+        id=request.data['id']
+        streetlight = StreetLight.objects.get(id=id)
+        if streetlight == None:
+            return Response({'message':'Cannot update data does not exitst'}, status=status.HTTP_400_BAD_REQUEST)
+        streetlight.uptime = request.data['uptime']
+        streetlight.status = request.data['status']
+        streetlight.save()
+        return Response({'message': 'Data updated successfully'}, status=status.HTTP_200_OK)
+        
+    
     
 class PolePowerAPI(APIView):
+    #to save poles power at each instance
     def post(self, request):
-        # Save new power data
-        pass
+        id = request.data['id']
+        power = request.data['power']
+        print(request.data['id'])
+        streetlight = StreetLight.objects.get(id=id)
+        n = PolePower.objects.create(streetLight=streetlight, power=power)
+        serializer = PolePowerSerializer(n)
+        return Response({'data':serializer.data, 'message': 'Data Saved Successfully'}, status=status.HTTP_201_CREATED)
+        # pass
