a300762f7e5e1ae7d1d08e94b2feffbc04e659fb
biohazard2117 <uttu.college.id@gmail.com>
2023-02-12 02:21:41 +0530

    Mailing functionality

--- backend/SLIMS/settings.py
+++ backend/SLIMS/settings.py
@@ -129,3 +129,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'skive50@gmail.com'
+EMAIL_HOST_PASSWORD = 'mhnynuweaafdrvgh'
+RECIPIENT_EMAIL = 'uttu.college.id@gmail.com'
