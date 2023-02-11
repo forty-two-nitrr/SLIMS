9472fee0f02da92742eae1ab872361c8b420b908
Pratham Gandhi <ppg.1382@gmail.com>
2023-02-11 14:58:43 +0530

    added bounding box detection

--- backend/detector/management/commands/detect.py
+++ backend/detector/management/commands/detect.py
@@ -7,8 +7,9 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         # model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
         # model.classes = list(range(10))
-
-        url = "http://192.168.237.1:6969/stream"
+        url = "http://192.168.177.1:81/stream"
+        model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+        model.classes = list(range(9))
 
         cap = cv2.VideoCapture(url)
         if not cap.isOpened():
@@ -16,10 +17,16 @@ class Command(BaseCommand):
             exit()
         while True:
             ret, frame = cap.read()
-
+            results = model(frame)
             if not ret:
                 print("Can't receive frame, exiting...")
                 break
+            for box in results.xyxy[0]:
+                xB = int(box[2])
+                xA = int(box[0])
+                yB = int(box[3])
+                yA = int(box[1])
+                cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
             frame = cv2.resize(frame, (640, 480))
             cv2.imshow('frame', frame)
             if cv2.waitKey(1) == ord('q'):
