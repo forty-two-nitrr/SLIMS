4fcec2ebc9b2a008d71291451ba36c289214818e
Pramil Kesarwani <kesarwanipramil01@gmail.com>
2023-02-12 00:02:59 +0530

    Map added

--- slims-frontend/src/components/Map/index.js
+++ slims-frontend/src/components/Map/index.js
@@ -1,10 +1,48 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
+import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+
 import "./Map.css";
 
+mapboxgl.accessToken =
+  "pk.eyJ1IjoicHJhbWlsMDEiLCJhIjoiY2wwY3MzaGxrMDI2aDNqcDlxcTY2ZHhjZCJ9.tBxkMn02CfdKVlWFVWm51Q";
+
 const Map = () => {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(81.6049);
+  const [lat, setLat] = useState(21.2494);
+  const [zoom, setZoom] = useState(18);
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v12",
+      center: [lng, lat],
+      zoom: zoom,
+    });
+    map.current.scrollZoom.disable();
+  });
+
+  useEffect(() => {
+    if (!map.current) return; // wait for map to initialize
+    map.current.on("move", () => {
+      setLng(map.current.getCenter().lng.toFixed(4));
+      setLat(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
+    });
+  });
+
   return (
     <div className="map-container">
-      <div className="map-inner-container"></div>
+      <div className="map-inner-container">
+        <div>
+          <div className="sidebar">
+            Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+          </div>
+          <div ref={mapContainer} className="map" />
+        </div>
+      </div>
       <div className="details-container"></div>
     </div>
   );
