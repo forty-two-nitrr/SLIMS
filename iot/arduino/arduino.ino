07d206b57447a1b11292e0e43df5641a9b3972b9
rikku <rikku.genius@gmail.com>
2023-02-12 14:17:05 +0530

    p2p

---
+++ iot/arduino/arduino.ino
@@ -0,0 +1,17 @@
+#include <SoftwareSerial.h>
+#include <ArduinoJson.h>
+SoftwareSerial s(5,6);
+
+void setup() {
+  s.begin(9600);
+}
+
+void loop() {
+  StaticJsonBuffer<1000> jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject();
+  root["data1"] = 100;
+  root["data2"] = 200;
+  if(s.available()>0){
+    root.printTo(s);
+  }
+}
--- iot/rbt.h
+++
@@ -1,7 +0,0 @@
-#ifndef RBT_H
-#define RBT_H
-
-void	insertion	(int data);
-void	deleteion	(int data);
-
-#endif
